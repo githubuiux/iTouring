c7f78d9a9b1217d28b80c3ff5578cb9f99faa0c4
githubuiux <uiuxista@gmail.com>
2023-11-10 11:03:46 +0300

    Initial Commit

---
+++ iTouring/iTouringApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTouringApp.swift
+//  iTouring
+//
+//  Created by Levent on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTouringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
